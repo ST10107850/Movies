c2b219264f58fbc70df77cb61538ec9f01b20a57
chailock <chailock.sphe@gmail.com>
2024-08-19 10:51:46 +0200

    Adding tailwind and Navbar Page

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <div className="top-0 m-0 z-10 w-full">
+        <nav className="max-w-[1600px] bg-transparent">
+            <a href="#">Enter-Stream</a>
+        </nav>
+
+    </div>
+  )
+}
+
+export default NavBar
